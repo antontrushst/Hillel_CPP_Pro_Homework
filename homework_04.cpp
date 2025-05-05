5f25d5b051a931472cc8aa13a152dfcc1bca83ee
Anton Trush <anton.st.trush@gmail.com>
2025-05-05 16:36:45 +0300

    Add finished 4th homework

--- homework_04.cpp
+++ homework_04.cpp
@@ -1,7 +1,12 @@
+// Програма відтворює простий логер для відпрацювання патернів проектування.
+
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <memory>
+#include <cctype>
+#include <algorithm>
+#include <map>
 
 struct LogSink
 {
@@ -45,12 +50,14 @@ struct FileSink: public LogSink
             log_file.open(file_name, std::ios::app);
             log_file << msg << std::endl;
             log_file.close();
+            std::cout << "NOTE: \"app.log\" was succesfully updated" << std::endl;
         }
         else
         {
             std::ofstream log_file(file_name);
             log_file << msg << std::endl;
             log_file.close();
+            std::cout << "NOTE: \"app.log\" was succesfully created" << std::endl;
         }
     }
 };
@@ -75,9 +82,17 @@ public:
     {
         switch(type)
         {
-            case SinkType::CONSOLE: sink_.reset(new ConsoleSink());
-            case SinkType::FILE: sink_.reset(new FileSink());
-            case SinkType::NONE: sink_.reset(new NullSink());
+            case SinkType::CONSOLE:
+                sink_.reset(new ConsoleSink());
+                break;
+            case SinkType::FILE:
+                sink_.reset(new FileSink());
+                break;
+            case SinkType::NONE:
+                sink_.reset(new NullSink());
+                break;
+            default:
+                sink_.reset(new NullSink());
         }
     }
 
@@ -95,12 +110,59 @@ private:
 
 
 
-int main()
+int main(int argc, char** argv)
 {
+    std::string type;
+
+    if(argc > 1)
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            type = argv[i];
+            std::transform(type.begin(), type.end(), type.begin(), ::toupper);
+        }
+    }
+
     std::string message = "ping!";
     auto& logger = Logger::instance();
-    logger.set_sink(SinkType::CONSOLE);
+
+    // Мапа для пошуку відповідності аргументу від користувача зі списком типів.
+    std::map<std::string, SinkType> mp;
+    mp["CONSOLE"] = SinkType::CONSOLE;
+    mp["FILE"] = SinkType::FILE;
+    mp["NONE"] = SinkType::NONE;
+    std::map<std::string, SinkType>::iterator it;
+    it = mp.find(type);
+
+    // Пошук по мапі типів
+    switch(it->second)
+    {
+        case SinkType::CONSOLE:
+            logger.set_sink(SinkType::CONSOLE);
+            break;
+        case SinkType::FILE:
+            logger.set_sink(SinkType::FILE);
+            break;
+        case SinkType::NONE:
+            logger.set_sink(SinkType::NONE);
+            std::cout << "NOTE: SinkType is set to NONE." << std::endl;
+            break;
+        default:
+            std::cout << "WARNING: No argument provided or your argument is invalid!\n"
+                      << "\tSetting SinkType to CONSOLE." << std::endl;
+            logger.set_sink(SinkType::CONSOLE);
+    }
+
     logger.log(message);
+
+    // Визначені домашкою перевірки.
+    std::cout << "\n\nFollowing examples are predifined in code:\n";
+    Logger::instance().set_sink(SinkType::FILE);
+    Logger::instance().log("Test message... This message goes to the \"app.log\" file.\n");
+    Logger::instance().set_sink(SinkType::NONE);
+    Logger::instance().log("nothing will happen");
+    Logger::instance().set_sink(SinkType::CONSOLE);
+    Logger::instance().log("Test message... Goes to the console.\n");
 
     return 0;
 }
