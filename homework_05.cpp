829023983ef809e470979388af8bd478105ca9c2
Anton Trush <anton.st.trush@gmail.com>
2025-05-24 16:02:44 +0300

    Homework 5 (in process 01)

---
+++ homework_05.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+#include <fstream>
+#include <functional>
+#include <map>
+
+struct INumberReader
+{
+    virtual ~INumberReader() = default;
+    virtual std::vector<int> read(const std::string& file_name) = 0;
+};
+
+struct NumberReader: public INumberReader
+{
+    std::vector<int> numbers;
+    bool is_integer(std::string& input_str) const
+    {
+        for(int i = 0; i < input_str.size(); i++)
+            if(input_str[i] == '-' && i != 0 ||                 // checking if a character is not minus at the beginning
+               input_str[i] < '0' && input_str[i] != '-' ||     // checking if a character is not ASCII zero or greater or minus
+               input_str[i] > '9' && input_str[i] != '-')       // checking if a character is not ASCII nine or lesser or minus
+                return false;
+
+        return true;
+    }
+
+    std::vector<int> read(const std::string& file_name) override
+    {
+        std::ifstream input_file(file_name);
+
+        if(!input_file.is_open())
+        {
+            std::cerr << "ERROR: Couldn't open file!" << std::endl;
+            return this->numbers;
+        }
+
+        std::string temp;
+        while(input_file >> temp)
+        {
+            if(is_integer(temp))
+            {
+                numbers.push_back(std::stoi(temp));
+            }
+        }
+        return this->numbers;
+    }
+
+};
+
+struct INumberFilter
+{
+    virtual ~INumberFilter() = default;
+    virtual bool keep(int) const = 0;
+};
+
+struct OddFilter: public INumberFilter
+{
+    bool keep(int number) const override
+    {
+        return number % 2;
+    }
+};
+
+struct EvenFilter: public INumberFilter
+{
+    bool keep(int number) const override
+    {
+        return !(number % 2);
+    }
+};
+
+struct GreaterThanFilter: public INumberFilter
+{
+    int reference_number;
+    GreaterThanFilter(int ref_number) : reference_number(ref_number) {}
+
+    // Constractor that parses argv command argument
+    GreaterThanFilter(char* argv)
+    {
+        std::string ref_num = argv;
+        ref_num.erase(ref_num.begin(), ref_num.begin()+2);
+        reference_number = std::stoi(ref_num);
+    }
+
+    bool keep(int number) const override
+    {
+        return number > reference_number;
+    }
+};
+
+struct INumberObserver
+{
+    virtual ~INumberObserver() = default;
+    virtual void on_number(int) = 0;
+    virtual void on_finished() = 0;
+};
+
+struct PrintObserver: public INumberObserver
+{
+    void on_number(int number) override
+    {
+        std::cout << number << std::endl;
+    }
+};
+
+struct CountObserver: public INumberObserver
+{
+    int counter = 0;
+
+    void on_finished() override
+    {
+        std::cout << "Counted " << counter << " numbers in provided file.\n";
+    }
+};
+
+class FilterFactory
+{
+    using Factory = std::function<std::unique_ptr<INumberFilter>(std::istream&)>;
+    std::map<std::string, Factory> filter_registry;
+
+    FilterFactory() = default;
+    FilterFactory(const FilterFactory& other) = delete;
+    FilterFactory& operator=(FilterFactory& other) = delete;
+
+public:
+    static FilterFactory& instance()
+    {
+        static FilterFactory registry;
+        return registry;
+    }
+
+    void register_filter(const std::string& filter_type, Factory f)
+    {
+        this->filter_registry[filter_type] = f;
+    }
+
+    std::unique_ptr<INumberFilter> create(const std::string& filter_type, std::istream& is)
+    {
+        return this->filter_registry[filter_type](is);
+    }
+};
+
+int main(int argc, char** argv)
+{
+    // Throw a note on program usage if the required arguments haven't been provided, and close the program.
+    if(argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <filter>" << " <file_name>\n"
+                  << "where\n\t<filter> - name of a filter (ODD, EVEN, GT<n>);\n"
+                  << "\t<file_name> - name of the text file with numbers;";
+        return -1;
+    }
+
+    FilterFactory& filter_factory = FilterFactory::instance();
+    filter_factory.register_filter("ODD", [](std::istream& is)
+    {
+        return std::make_unique<OddFilter>();
+    });
+    filter_factory.register_filter("EVEN", [](std::istream& is)
+    {
+        return std::make_unique<EvenFilter>();
+    });
+    filter_factory.register_filter("GT", [argv](std::istream& is)
+    {
+        return std::make_unique<GreaterThanFilter>(argv[1]);
+    });
+
+    
+    NumberReader num_read;
+    // GreaterThanFilter gt(argv[1]);
+    // for (auto i : num_read.read(argv[2]))
+    // {
+    //     std::cout << gt.keep(i) << std::endl;
+    // }
+
+    std::vector<int> numbers = num_read.read(argv[2]);
+    
+    return 0;
+}
